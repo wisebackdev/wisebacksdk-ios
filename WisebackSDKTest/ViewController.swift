0d08fe5278a593f348b19539638e33bb8bd3981a
Ali Varlı <ali@feedbackie.com>
2018-04-19 15:33:32 +0300

    Initial Commit

---
+++ WisebackSDKTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WisebackSDKTest
+//
+//  Created by Ali Varli on 19.04.2018.
+//  Copyright © 2018 Wise Bilisim Teknolojileri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
