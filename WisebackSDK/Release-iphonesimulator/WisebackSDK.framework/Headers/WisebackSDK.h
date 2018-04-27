9078184efa04615c006b90beff84f484ca26a6b8
Ali Varlı <ali@feedbackie.com>
2018-04-27 17:10:48 +0300

    first commit

---
+++ WisebackSDK/Release-iphonesimulator/WisebackSDK.framework/Headers/WisebackSDK.h
@@ -0,0 +1,19 @@
+//
+//  WisebackSDK.h
+//  WisebackSDK
+//
+//  Created by Ali Varli on 19.04.2018.
+//  Copyright © 2018 Wise Bilisim Teknolojileri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WisebackSDK.
+FOUNDATION_EXPORT double WisebackSDKVersionNumber;
+
+//! Project version string for WisebackSDK.
+FOUNDATION_EXPORT const unsigned char WisebackSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WisebackSDK/PublicHeader.h>
+
+
--- WisebackSDKTest/ViewController.swift
+++ WisebackSDKTest/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import WisebackSDK
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WiseViewDelegate {
+    var wview:WiseView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showBtnClick(_ sender: Any) {
+        wview = WiseView(frame: self.view.bounds)
+        wview.delegateWise = self
+        wview.showForm(formID: "VgcfNL", targetView: self.view, animation: .Up)
+    }
+    
+    // MARK: - Delegate methods
+    func startLoading() {
+        print("Loading form started")
+    }
+    func formOpened() {
+        print("Form opened")
+    }
+    func errorLoading(error: Error) {
+        print("An error occurred: \(error.localizedDescription)")
+    }
+    func formClosed() {
+        print("Form closed")
+    }
 }
 
